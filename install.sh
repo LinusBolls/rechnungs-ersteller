69348c78ad63ede94ca18fbf7ec89319f95ce8d7
Linus Bolls <linus.bolls@code.berlin>
2022-05-09 20:30:49 +0200

    feat: made deployment work

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # download, extract and remove  git repo zip
-wget "https://github.com/linusbolls/rechnungs-ersteller/archive/refs/heads/master.zip" -O ~/temp.zip
-unzip temp.zip -d ~/Library/Application\ Support/RechnungsErsteller
+curl -L https://github.com/linusbolls/rechnungs-ersteller/archive/refs/heads/master.zip --output ~/temp.zip
+unzip ~/temp.zip -d ~/Library/Application\ Support/RechnungsErsteller
 rm ~/temp.zip
 
 # install dependencies from requirements.txt
-pip3 install -r ~/Library/Application\ Support/RechnungsErsteller/requirements.txt
+pip3 install -r ~/Library/Application\ Support/RechnungsErsteller/rechnungs-ersteller-master/requirements.txt
 
 # move startup script into home directory to make it more accessible via spotlight
-mv ~/Library/Application\ Support/RechnungsErsteller/launch.sh ~/Rechnungs\ Ersteller
+mv ~/Library/Application\ Support/RechnungsErsteller/rechnungs-ersteller-master/launch.sh ~/Der\ Schuldner
 # give startup script execution permissions
-chmod u+x ~/Rechnungs\ Ersteller
+chmod u+x ~/Der\ Schuldner
--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 ~/Library/Application\ Support/Rechner/src/index.py
+python3 ~/Library/Application\ Support/Rechnungs\ Ersteller/rechnungs-ersteller-master/src/index.py
